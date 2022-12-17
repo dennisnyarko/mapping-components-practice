c48886c682c51e126fa536b0e6ad0722bf1bc62b
Dennis Nyarko <dennis_nyarko@outlook.com>
2022-12-17 14:56:59 +0000

    map through emojipedia array

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,17 @@ import React from "react";
 import Entry from "./Entry";
 import emojipedia from "../emojipedia";
 
+function createEntry(emojiTerm) {
+  return (
+  <Entry
+    key={emojiTerm.id}
+    emoji={emojiTerm.emoji}
+    name={emojiTerm.name}
+    description={emojiTerm.meaning}
+   />
+   )
+}
+
 function App() {
   return (
     <div>
@@ -10,13 +21,7 @@ function App() {
       </h1>
 
       <dl className="dictionary">
-        <Entry 
-          emoji="💪"
-          name="Tense Biceps"
-          description="“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
-        />
-        <Entry />
-        <Entry />
+        {emojipedia.map(createEntry)}
         
       </dl>
     </div>
